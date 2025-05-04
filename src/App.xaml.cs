791624906f29e81a7feae2d587a9e2270aa1cc61
David Koteksi <142885534+seakyy@users.noreply.github.com>
2025-05-04 22:04:59 +0200

    version 1.0
    Working Application

---
+++ src/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace calculate_pi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
